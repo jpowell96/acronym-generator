39f0b65519cb9c410851797fde8298c3b1b5908a
Jonathan Powell <powell.jonj@gmail.com>
2022-09-05 22:33:16 -0400

    initial pass open telemetry

---
+++ src/main/java/acronym/generator/OpenTelemetryConfig.java
@@ -0,0 +1,15 @@
+package acronym.generator;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenTelemetryConfig {
+
+    @Bean
+    public OpenTelemetry openTelemetry() {
+        return GlobalOpenTelemetry.get();
+    }
+}
--- src/main/java/acronym/generator/controller/AcronymController.java
+++ src/main/java/acronym/generator/controller/AcronymController.java
@@ -1,6 +1,8 @@
 package acronym.generator.controller;
 
 import acronym.generator.service.AcronymService;
+import io.opentelemetry.api.OpenTelemetry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,12 +12,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/acronym")
+// TODO: Keep following this example - https://github.com/open-telemetry/opentelemetry-java-docs/tree/03099e4e39cb307f19ea19217ff381e07c9284ce/javaagent
 public class AcronymController {
-    AcronymService acronymService;
+    private final AcronymService acronymService;
+    private final OpenTelemetry openTelemetry;
 
-    public AcronymController(AcronymService acronymService) {
+    @Autowired
+    public AcronymController(AcronymService acronymService, OpenTelemetry openTelemetry) {
 
         this.acronymService = acronymService;
+        this.openTelemetry = openTelemetry;
         System.out.println("Hello");
         System.out.println("Test");
     }
